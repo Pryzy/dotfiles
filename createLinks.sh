e253db6d3babb953078d29d4e57ec988925a691e
Piotr Ryzynski <p.ryzynski@gmail.com>
2023-12-19 22:35:55 +0100

    Remove old stuff - prepare for the new one

--- createLinks.sh
+++ createLinks.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-ln ./vimrc ~/.vimrc
 ln ./gitconfig ~/.gitconfig
-ln ./i3status.conf /etc/i3status.conf
-ln ./i3config ~/.config/i3/config
-cp ./.tmux.conf ~/
-cp ./.tmux.conf.local ~/
