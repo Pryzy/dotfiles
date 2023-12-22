bb0b81f141090b7428190e3d1e57c9947eee5d05
Piotr Ryzynski <p.ryzynski@gmail.com>
2023-12-22 13:41:01 +0100

    Add neovim packer install during link creation

--- createLinks.sh
+++ createLinks.sh
@@ -7,4 +7,7 @@ mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -s $(pwd)/.tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
 
 # Install Neovim
+# Install Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 ln -s $(pwd)/nvim $XDG_CONFIG_HOME/nvim
