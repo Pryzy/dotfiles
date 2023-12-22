184bd6e2cac004837bc23e4dd07175fdae84fcb6
Piotr Ryzynski <p.ryzynski@gmail.com>
2023-12-22 13:22:46 +0100

    Fix createLinks script to finally work with symbolic link

--- createLinks.sh
+++ createLinks.sh
@@ -4,4 +4,7 @@ ln ./gitconfig ~/.gitconfig
 
 # Install Tmux
 mkdir -p "$XDG_CONFIG_HOME/tmux"
-ln "./.tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+ln -s $(pwd)/.tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
+
+# Install Neovim
+ln -s $(pwd)/nvim $XDG_CONFIG_HOME/nvim
