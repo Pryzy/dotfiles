075a2769a9b12a7bd881e464db077fb717a0245b
Piotr Ryzynski <p.ryzynski@gmail.com>
2023-12-27 11:50:36 +0100

    Add nvim tree

--- nvim/lua/pryzy/packer.lua
+++ nvim/lua/pryzy/packer.lua
@@ -63,4 +63,11 @@ return require('packer').startup(function(use)
 		  {'L3MON4D3/LuaSnip'},
 	  }
   }
+
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+}
 end)
