dea69662d6213474139c7779657f402775d6d2b2
Piotr Ryzynski <p.ryzynski@gmail.com>
2023-12-29 11:00:43 +0100

    Add configs + keybindings

--- nvim/after/plugin/undotree.lua
+++ nvim/after/plugin/undotree.lua
@@ -1,2 +1,1 @@
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-vim.cmd('source $HOME/.config/nvim/after/plugin/undotree.vim')
--- nvim/lua/pryzy/remap.lua
+++ nvim/lua/pryzy/remap.lua
@@ -1,2 +1,60 @@
+-- Keybindings
 vim.g.mapleader = " "
 vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
+    -- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+    -- Append with cursor not moving
+vim.keymap.set("n", "J", "mzJ`z")
+    -- Navigation keybindings
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+    -- Copy to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+Y")
+    -- Delete to void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+    -- Replace selected word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Line numbering
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Tabs/spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Don't wrap lines
+vim.opt.wrap = false
+
+-- Swap/Backup/Undo files
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Search options
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Colors!
+vim.opt.termguicolors = true
+
+-- Keep 8 lines discance between top-bottom scroll
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Update time
+vim.opt.updatetime = 50
+
+-- Set 80th column line
+vim.opt.colorcolumn = "80"
