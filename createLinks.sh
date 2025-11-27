525278a9516934bebea030b719f621c553ce59e2
Piotr Ryzynski <p.ryzynski@gmail.com>
2025-11-27 08:13:00 +0100

    Update landing directory to place scripts

--- createLinks.sh
+++ createLinks.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ln ./gitconfig ~/.gitconfig
+ln -s $(pwd)/gitconfig $HOME/.gitconfig
 
 # Install Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-mkdir -p "$XDG_CONFIG_HOME/tmux"
-ln -s $(pwd)/.tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf
+mkdir -p "$HOME/.config/tmux"
+ln -s $(pwd)/.tmux.conf $HOME/.config/tmux/tmux.conf
 
 # Install Neovim
 # Install Packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-ln -s $(pwd)/nvim $XDG_CONFIG_HOME/nvim
+ln -s $(pwd)/nvim $HOME/.config/nvim
--- nvim/lua/pryzy/remap.lua
+++ nvim/lua/pryzy/remap.lua
@@ -1,24 +1,24 @@
 -- Keybindings
 vim.g.mapleader = " "
 vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
-    -- Move selected lines
+-- Move selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-    -- Append with cursor not moving
+-- Append with cursor not moving
 vim.keymap.set("n", "J", "mzJ`z")
-    -- Navigation keybindings
+-- Navigation keybindings
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-    -- Copy to clipboard
+-- Copy to clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>y", "\"+Y")
-    -- Delete to void register
+-- Delete to void register
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
-    -- Replace selected word
+-- Replace selected word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Line numbering
